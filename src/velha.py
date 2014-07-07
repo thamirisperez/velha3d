aa85cddf4d315572464012d2cdf99dbfcc67b5d4
ThamirisSouza <thamirissouza28@hotmail.com>
2014-07-07 18:47:30 +0000

    em 3d com bolas e quadrodos azuis e vermelhos

--- src/velha.py
+++ src/velha.py
@@ -1,17 +1,17 @@
 from glow import *
-_gs=glow('main')
+_gs = glow('main')
 cena = canvas()
 cena.width = 400
 cena.height = 400
-#box(color=color.blue)
-#box(size=(2,2,2),opacity=0.2)
-#sphere(pos=(2,2,2), color =color.red)
+TAM = (-1, 0, 1)
+TABULEIRO = []
+PECAS = []
 print("oi")
-pecas =[(box,color.blue), (sphere, color.red)] * 5
-for linha in range(3):
-   for coluna in range(3):
-       for camada in range(3):
-        box(pos=(coluna*3,linha *3,camada *3), size=(2,2,2), opacity=0.2)
-#for linha in range(3):
-   #for coluna in range(3):
-       #box(pos=(coluna*3,0,0), size=(2,2,2), opacity=0.2)
+pecas = [box,sphere] *14
+cores = [color.red,color.blue] *14
+TABULEIRO = [box(pos=(coluna*3, linha*3,camada *3 ), size=(2, 2, 2), opacity=0.2)
+             for coluna in TAM for linha in TAM for camada in TAM]
+cor = color.blue
+#peca, cor = pecas.pop()
+PECAS = [pecas.pop()(pos=(coluna*3, linha*3, camada *3), color=cores.pop(),  size=(1, 1, 1), opacity=0.6)
+             for coluna in TAM for linha in TAM for camada in TAM]
